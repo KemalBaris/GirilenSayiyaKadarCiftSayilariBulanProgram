91196408e24a2bf1330248aaa7b33e9d997edb0e
Barış <kemalbariscakir@gmail.com>
2022-11-02 11:54:41 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        int k;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Sayı Giriniz! : ");
+        k = input.nextInt();
+
+        for (int i = 1; i<=k; i++){
+            if (i % 2 == 0){
+                System.out.println(i);
+
+                /*  *ÖDEV*
+                for (int i = 1; i<=k; i++){
+                    if (i % 2 == 0  && i % 4 == 0){
+                        System.out.println(i)
+                 */
+            }
+        }
+
+    }
+}
